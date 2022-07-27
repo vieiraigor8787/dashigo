e62372e8ff7f97a9a42d325d14cb05dd4276d338
igor_cerqueira_jussi <igor_cerqueira_jussi@whirlpool.com>
2022-07-27 16:36:18 -0300

    axios config

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -22,13 +22,13 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
 
 export default function UserList() {
   const { data, error, isLoading, isFetching } = useQuery(["users"], async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
+    const { data } = await api.get('users');
 
-    const users = data.users.map(user => {
+    const users = data.users.map((user) => {
       return {
         id: user.id,
         name: user.name,
