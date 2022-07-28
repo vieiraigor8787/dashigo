ffc2268e472b294657a37ace3e874e214f87559e
igor_cerqueira_jussi <igor_cerqueira_jussi@whirlpool.com>
2022-07-28 11:19:20 -0300

    config pagination on mirajets

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,47 +1,69 @@
-import { createServer, Factory, Model } from "miragejs";
-import { faker } from '@faker-js/faker';
+import { createServer, Factory, Model, Response } from "miragejs";
+import { faker } from "@faker-js/faker";
 
 type User = {
   name: string;
   email: string;
   created_at: string;
-}
+};
 
-export function makeServer(){
-    const server = createServer({
-      models: {
-        user: Model.extend<Partial<User>>({})
-      },
+export function makeServer() {
+  const server = createServer({
+    models: {
+      user: Model.extend<Partial<User>>({}),
+    },
 
-      factories: {
-        user: Factory.extend({
-          name(i: number) {
-            return `User ${i + 1}`
+    factories: {
+      user: Factory.extend({
+        name(i: number) {
+          return `User ${i + 1}`;
+        },
+        email() {
+          return faker.internet.email().toLocaleLowerCase();
+        },
+        createdAt() {
+          return faker.date.recent(10);
+        },
+      }),
+    },
+
+    seeds(server) {
+      server.createList("user", 200);
+    },
+
+    routes() {
+      this.namespace = "api";
+      this.timing = 750;
+
+      this.get("/users", function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
+
+        //saber quantos registros tem na listagem
+        const total = schema.all("user").length;
+
+        //calc inicio e final de cada pagina
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all("user")).users.slice(
+          pageStart,
+          pageEnd
+        );
+
+        return new Response(
+          200,
+          {
+            "x-total-count": String(total)
           },
-          email() {
-            return faker.internet.email().toLocaleLowerCase();
-          },
-          createdAt() {
-            return faker.date.recent(10);
-          },
-        })
-      },
+          { users }
+        );
+      });
+      this.post("/users");
 
-      seeds(server) {
-        server.createList('user', 10)
-      },
-      
-      routes() {
-        this.namespace = 'api';
-        this.timing = 750;
+      this.namespace = "";
+      this.passthrough();
+    },
+  });
 
-        this.get('/users');
-        this.post('/users');
-
-        this.namespace = '';
-        this.passthrough();
-      }
-    })
-
-    return server;
+  return server;
 }
