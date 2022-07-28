c1165a4d39c1c33fa6a0c03eaf75a660c87d9953
igor_cerqueira_jussi <igor_cerqueira_jussi@whirlpool.com>
2022-07-28 12:44:22 -0300

    using mutation for create user

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -7,7 +7,6 @@ import {
   HStack,
   SimpleGrid,
   Stack,
-  Text
 } from "@chakra-ui/react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -16,6 +15,8 @@ import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import Link from "next/link";
+import { useMutation } from "@tanstack/react-query";
+import { api } from "../../services/api";
 
 type CreateUserFormData = {
   name: string;
@@ -37,6 +38,17 @@ const CreateUserFormSchema = yup.object({
 });
 
 export default function CreateUser() {
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post('users', {
+      user: {
+        ... user,
+        created_at: new Date(),
+      }
+    })
+
+    return response.data.user;
+  })
+
   const {
     register,
     handleSubmit,
@@ -48,7 +60,7 @@ export default function CreateUser() {
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (
     values
   ) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await createUser.mutateAsync(values);
 
   };
 
